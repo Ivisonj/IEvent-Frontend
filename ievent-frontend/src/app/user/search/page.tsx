e94e8cf9cecacaff9bc1ba8f321a2d1ab0c48c8b
Ivison Joel <115274427+Ivisonj@users.noreply.github.com>
2024-05-01 00:44:22 -0300

    feat: search page card creation

--- ievent-frontend/src/app/user/search/page.tsx
+++ ievent-frontend/src/app/user/search/page.tsx
@@ -1,5 +1,30 @@
+import { Box, Typography } from '@mui/material'
+
+import SearchPageCard from '@/components/searchPageCard'
+
+const eventData = [
+  {
+    id: '01',
+    title: 'Ensaio Coral Jovem de Petrolina',
+    createdBy: 'Ivison Joel',
+  },
+]
+
 const SearchPage = () => {
-  return <h1>Search</h1>
+  return (
+    <Box width="100%" flexDirection="column">
+      <Typography variant="h5" mb={1}>
+        Buscar Evento
+      </Typography>
+      {eventData.map((item) => (
+        <SearchPageCard
+          key={item.id}
+          title={item.title}
+          createdBy={item.createdBy}
+        />
+      ))}
+    </Box>
+  )
 }
 
 export default SearchPage
